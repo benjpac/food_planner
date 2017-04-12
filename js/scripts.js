9d470c215b53339493f7471f942d4a5039f8617c
Ben Metzger and Chitroopa Manikkavasagam <ben@>
2017-04-12 14:37:27 -0700

    fix bug with ingredient match function

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@ var recipeDB = [
     "name" : "quick_colcannon",
     "image" : "http://www.traderjoes.com/images/recipes/353MainImage.jpg",
     "ingredients" : [
-      "uncured_apple_smoked_bacon",
-      "frozen_Mashed_Potatoes",
-      "Water",
-      "cruciferous_crunch",
+      "bacon",
+      "potatoe",
+      "mixed_greens",
       "butter"
     ],
     "cuisine" : "american",
@@ -21,13 +20,13 @@ var recipeDB = [
       "6 slices Uncured Apple Smoked Bacon",
       "1 bag Frozen Mashed Potatoes",
       "1/3 cup Water, plus 2 tablespoons",
-      "1/2 bag Cruciferous Crunch Collection",
+      "1/2 bag Mixed Greens",
       "Kerrygold Pure Irish Butter"
     ],
     "directionsList" : [
       "In a large frying pan on medium heat, cook 6 slices of bacon. Set cooked bacon aside on a paper towel-lined plate. Discard all but 1 tablespoon of oil from the pan.",
       "Add bag of frozen mashed potato medallions and 1/3 cup of water. Cook over moderate heat, stirring frequently.",
-      "After about 5 minutes, once potatoes begin to soften, add 1/2 bag cruciferous crunch and 2 additional tablespoons of water. Stir then cover to trap steam.",
+      "After about 5 minutes, once potatoes begin to soften, add 1/2 bag mixed greens and 2 additional tablespoons of water. Stir then cover to trap steam.",
       "Cook for another 2-3 minutes or so, stirring every so often, until greens soften a bit.",
       "Chop cooled bacon into small pieces then add to pan of potatoes. Serve right away with a pat of butter on top."
     ]
@@ -37,12 +36,12 @@ var recipeDB = [
     "name" : "green_curry_chickpeas",
     "image" : "http://www.traderjoes.com/images/recipes/411MainImage.jpg",
     "ingredients" : [
-      "organic_garbanzo_beans",
+      "garbanzo_bean",
       "green_curry_sauce",
-      "organic_baby _pinach",
-      "organic_red_cherry_tomatoes",
-      "organic_quinoa",
-      "sea_salt",
+      "baby_spinach",
+      "red_cherry_tomatoe",
+      "quinoa",
+      "salt",
       "pepper",
       "tandoori_masala",
       "olive_oil"
@@ -64,7 +63,8 @@ var recipeDB = [
       "Pepper",
       "Olive oil"
     ],
-      "directionsList" : [ "Preheat oven to 400°F",
+      "directionsList" : [
+      "Preheat oven to 400°F",
       "Toss the tomatoes lightly with olive oil, salt and pepper and spread them out into one layer on a sheet pan.",
       "Roast for 15 to 20 minutes, until the tomatoes are soft. While tomatoes are roasting, prepare quinoa.",
       "Warm the curry sauce in a large saucepan over medium heat.",
@@ -77,15 +77,19 @@ var recipeDB = [
     "name" : "classic_italian_lasagna",
     "image" : "http://www.forgottothink.com/wp-content/uploads/2016/06/italian-dishes-6.jpg",
     "ingredients" : [
-      "minced_beef",
+      "beef",
       "onion",
       "butter",
-      "carrots",
-      "butter",
-      "wheat_flour",
+      "carrot",
+      "flour",
       "basil",
-      "lasagna_noodles",
-      "parmesan",
+      "lasagna_noodle",
+      "parmesan_cheese",
+      "milk",
+      "nutmeg",
+      "tomato_paste",
+      "olive_oil",
+      "celery"
     ],
     "cuisine" : "italian",
     "time" : 30,
@@ -101,17 +105,18 @@ var recipeDB = [
       "1.4 oz (40 g) celery ribs",
       "1.4 oz (40 g) tomato paste",
       "14.1 oz (400 ml) water",
-      "1.4 oz (40 g) wheat flour",
+      "1.4 oz (40 g) flour",
       "1.4 oz (40 g) butter",
       "26.5 oz (750 ml) milk 3.2%",
       "a pinch of nutmeg powder",
       "4 twigs of fresh basil",
       "2 pinches of salt",
       "1 tablespoon of olive oil",
-      "2.8 oz (80 g) parmesan",
+      "2.8 oz (80 g) parmesan cheese",
       "8 lasagna noodles"
     ],
-      "directionsList" : [ "Preparation of the sauce — Bolognese. Mix tomato paste with some hot water. Dice onion, carrots, and celery. Lay out vegetables and minced beef in a stew pan, pour in the tomato sauce. Add salt and pepper, scald, cover, and simmer on medium heat for 30 minutes. Add finely chopped basil 2-3 minutes before the sauce is done.",
+    "directionsList" : [
+      "Preparation of the sauce — Bolognese. Mix tomato paste with some hot water. Dice onion, carrots, and celery. Lay out vegetables and minced beef in a stew pan, pour in the tomato sauce. Add salt and pepper, scald, cover, and simmer on medium heat for 30 minutes. Add finely chopped basil 2-3 minutes before the sauce is done.",
       "Preparation of the sauce — Bechamel. Pour the milk into a saucepan and heat without bringing to boil. The milk should be hot. Melt the butter in a stewpan, add the flour, and fry for 2-3 minutes. Then, gradually pour in the hot milk, stirring to avoid lumps. Continue to stir and simmer, add salt and nutmeg. Once the sauce has thickened, it’s ready. Remove it from heat.",
       "Laying out the lasagna. Grate cheese. Preheat oven to 428°F (220°C). Oil a casserole dish and pour in some Bechamel sauce to cover the bottom, then pour in some Bolognese sauce following the same principle.",
       "Lay out 4 lasagna noodles with overlapping joints. Pour the sauces on top, and sprinkle with cheese. Repeat this process until ingredients are gone.",
@@ -123,9 +128,10 @@ var recipeDB = [
     "name" : "sweet_balls_of_fire",
     "image" : "http://www.traderjoes.com/images/recipes/472MainImage.jpg",
     "ingredients" : [
-      "organic_reduced_sugar_raspberry_preserves",
+      "sugar",
+      "raspberry_preserves",
       "sweet_chili_sauce",
-      "turkey_meatballs",
+      "turkey_meatball",
     ],
     "cuisine" : "american",
     "time" : 40,
@@ -140,53 +146,24 @@ var recipeDB = [
       "2 jars Sweet Chili Sauce",
       "2 packages Turkey Meatballs"
     ],
-      "directionsList" : [ "Preheat oven to 350°.",
+    "directionsList" : [
+      "Preheat oven to 350°.",
       "Prepare sauce: In a sauce pan over medium heat, add raspberry preserves and sweet chili sauce. Heat until sauce begins to bubble, about 8-10 minutes, stirring occasionally. Remove from heat.",
       "Place meatballs in a 9x9 baking dish and cover with warm sauce mixture. Place dish in oven and heat for 40 minutes, or until meatballs are heated to 165⩝F and sauce is hot. Alternatively, use a slow cooker to heat meatballs and sauce, about 5-6 hours on low, or 2-4 hours on high until heated through."
     ]
   },
-
   {
     "id": 5,
-    "name" : "biscuits_&_gravy_casserole",
-    "image" : "http://www.traderjoes.com/images/recipes/465MainImage.jpg",
-    "ingredients" : [
-      "ground-pork",
-      "flour",
-      "milk",
-    ],
-    "cuisine" : "american",
-    "time" : 35,
-    "calories" : 1500,
-    "vegetarian" : false,
-    "breakfast" : true,
-    "lunch": false,
-    "dinner" : false,
-      "ingredientsList" : [ "1 package (1 pound) TJ's Ground Pork",
-      "1/3 cup All Purpose Flour",
-      "3 cups Whole Milk",
-      "1/2 teaspoon Salt",
-      "1/2 teaspoon Pepper",
-      "Extra Virgin Olive Oil Spray, to grease pan",
-      "6 TJ's Eggs",
-      "1/2 teaspoon 21 Seasoning Salute",
-      "1 can TJ's Buttermilk Biscuits",
-      "1 cup TJ's Shredded Cheese Blend"
-    ],
-      "directionsList" : [ "In a large skillet over medium-high heat, brown the ground pork, breaking it up into pieces with a spoon as it cooks, about 8-10 minutes. When the pork has reached 145ºF and is no longer pink, reduce heat to medium. Sprinkle half of the flour over pork and stir to combine. Add remaining flour and stir to combine. Add one cup of milk to the pan and stir. Continue to add milk one cup at a time as gravy thickens. Cook for another 5-8 minutes to further thicken the gravy.",
-      "Preheat oven to 350ºF. Grease a 9x13 pan with olive oil spray and set aside. In a large bowl, add eggs and 21 Seasoning Salute and whisk to combine; set aside. Open the biscuits and tear each biscuit into four pieces. Line the bottom of the greased pan with half of the biscuit dough. Pour egg mixture on top of biscuit dough. Sprinkle cheese evenly atop egg mixture. Pour sausage gravy over egg mixture and top with remaining biscuit dough pieces.",
-      "Insert pan in oven and bake for 20 minutes. Cover pan with foil and bake for another 12-15 minutes. Remove from oven and allow to cool slightly before serving."
-    ]
-  },
-
-  {
-    "id": 5,
-    "name" : "biscuits_&_gravy_casserole",
+    "name" : "biscuits_and_gravy_casserole",
     "image" : "http://www.traderjoes.com/images/recipes/465MainImage.jpg",
     "ingredients" : [
       "ground_pork",
       "flour",
       "milk",
+      "olive_oil",
+      "cheddar_cheese",
+      "pepper",
+      "egg",
     ],
     "cuisine" : "american",
     "time" : 35,
@@ -195,56 +172,99 @@ var recipeDB = [
     "breakfast" : true,
     "lunch": false,
     "dinner" : false,
-      "ingredientsList" : [ "1 package (1 pound) TJ's Ground Pork",
+    "ingredientsList" : [
+      "1 package (1 pound) TJ's Ground Pork",
       "1/3 cup All Purpose Flour",
       "3 cups Whole Milk",
       "1/2 teaspoon Salt",
       "1/2 teaspoon Pepper",
       "Extra Virgin Olive Oil Spray, to grease pan",
       "6 TJ's Eggs",
-      "1/2 teaspoon 21 Seasoning Salute",
       "1 can TJ's Buttermilk Biscuits",
       "1 cup TJ's Shredded Cheese Blend"
     ],
-      "directionsList" : [ "In a large skillet over medium-high heat, brown the ground pork, breaking it up into pieces with a spoon as it cooks, about 8-10 minutes. When the pork has reached 145ºF and is no longer pink, reduce heat to medium. Sprinkle half of the flour over pork and stir to combine. Add remaining flour and stir to combine. Add one cup of milk to the pan and stir. Continue to add milk one cup at a time as gravy thickens. Cook for another 5-8 minutes to further thicken the gravy.",
+    "directionsList" : [
+      "In a large skillet over medium-high heat, brown the ground pork, breaking it up into pieces with a spoon as it cooks, about 8-10 minutes. When the pork has reached 145ºF and is no longer pink, reduce heat to medium. Sprinkle half of the flour over pork and stir to combine. Add remaining flour and stir to combine. Add one cup of milk to the pan and stir. Continue to add milk one cup at a time as gravy thickens. Cook for another 5-8 minutes to further thicken the gravy.",
       "Preheat oven to 350ºF. Grease a 9x13 pan with olive oil spray and set aside. In a large bowl, add eggs and 21 Seasoning Salute and whisk to combine; set aside. Open the biscuits and tear each biscuit into four pieces. Line the bottom of the greased pan with half of the biscuit dough. Pour egg mixture on top of biscuit dough. Sprinkle cheese evenly atop egg mixture. Pour sausage gravy over egg mixture and top with remaining biscuit dough pieces.",
       "Insert pan in oven and bake for 20 minutes. Cover pan with foil and bake for another 12-15 minutes. Remove from oven and allow to cool slightly before serving."
     ]
   },
   {
-    "id": 6,
-    "name" : "all_tired_up_thai_curry_soup",
-    "image" : "http://www.traderjoes.com/images/recipes/50MainImage.jpg",
+    "id" : 6,
+    "name" : "sausage_breakfast_sandwich",
+    "image" : "http://www.twopeasandtheirpod.com/wp-content/uploads/2013/12/Sausage-Egg-and-Cheese-Sandwich-with-Maple-Butter-3.jpg",
     "ingredients" : [
-      "quinoa",
-      "russet_potatoes",
-      "carrot_ginger_soup",
-      "fire_roasted_yellow_&_red_peppers"
-      "thai_yellow_curry_sauce"
+      "pork_sausage",
+      "egg",
+      "american_cheese",
+      "english_muffin",
+      "butter"
     ],
-    "cuisine" : "thai",
-    "time" : 30,
-    "calories" : 700,
-    "vegetarian" : true,
-    "breakfast" : false,
-    "lunch": true,
-    "dinner" : true,
-
-      "ingredientsList" : [ "1 cup TJ's Organic Quinoa",
-      "2 medium Russet Potatoes, peeled and diced into cubes",
-      "1 package Carrot Ginger Soup",
-      "2 jars Fire Roasted Yellow & Red Peppers",
-      "1 jar Thai Yellow Curry Sauce"
+    "cuisine" : "american",
+    "time" : 20,
+    "calories" : 383,
+    "vegetarian" : false,
+    "breakfast" : true,
+    "lunch": false,
+    "dinner" : false,
+    "ingredientsList" : [
+      "8 oz pork sausage",
+      "4 large eggs",
+      "4 slices American cheese",
+      "4 English muffins, halved",
+      "4 tablespoons salted butter, softened"
     ],
-      "directionsList" : [ "In a saucepan bring two cups of water to a boil; gently stir in the quinoa and diced potatoes.",
-      "Cover and reduce heat to a smooth boil.",
-      "Boil for 15 minutes, stirring occasionally. In another saucepan over medium-lowheat combine soup and fire roasted peppers.",
-      "Once the soup has warmed thoroughly, mix in the curry sauce. When the quinoa isdone, and the potatoes are soft, drain, and mix into the soup.",
-      "Cook together for an additional 5 minutes."
+    "directionsList" : [
+      "Form sausage into 4 patties. Cook in a large, non-stick sprayed skillet over medium-high heat for 3-4 minutes a side, or until golden brown and crisp. Place sausage patties on a paper towel-lined plate to drain the grease.",
+      "Drain fat from skillet then return to stove and turn heat down to medium. Whisk eggs, salt, and pepper in a bowl then pour into the skillet. Cook until top is nearly set but still glossy, then carefully fold eggs over in half. Remove from heat to finish cooking.",
+      "In a small bowl, whisk butter until creamy and smooth. Toast the English Muffins.",
+      "To assemble: Spread 1 tablespoon of maple butter on English Muffin, both halves. Place 1 egg on the bottom of a English muffin then top with cooked sausage patty and slice of cheese. Microwave for 20-30 seconds to melt cheese then cap with English muffin top. Continue making other sandwiches. Serve immediately."
     ]
   },
-
-
+  {
+    "id": 7,
+    "name" : "butter_chicken",
+    "image" : "http://noshingwiththenolands.com/wp-content/uploads/2016/03/Slow-Cooker-Butter-Chicken-square-Custom.jpg",
+    "ingredients" : [
+      "onion",
+      "garlic",
+      "tomato_sauce",
+      "heavy_cream",
+      "butter",
+      "cayenne_pepper",
+      "chicken",
+      "tandoori_masala",
+      "vegetable_oil",
+      "salt"
+    ],
+    "cuisine" : "indian",
+    "time" : 60,
+    "calories" : 384,
+    "vegetarian" : false,
+    "breakfast" : true,
+    "lunch": true,
+    "dinner" : true,
+    "ingredientsList" : [
+      "1 cup butter, divided",
+      "1 onion, minced",
+      "1 tablespoon minced garlic",
+      "1 (15 ounce) can tomato sauce",
+      "3 cups heavy cream",
+      "2 teaspoons salt",
+      "1 teaspoon cayenne pepper",
+      "1 teaspoon garam masala",
+      "1 1/2 pounds skinless, boneless chicken breast, cut into bite-sized chunks",
+      "2 tablespoons vegetable oil",
+      "2 tablespoons tandoori masala"
+    ],
+    "directionsList" : [
+      "Preparation of the sauce — Bolognese. Mix tomato paste with some hot water. Dice onion, carrots, and celery. Lay out vegetables and minced beef in a stew pan, pour in the tomato sauce. Add salt and pepper, scald, cover, and simmer on medium heat for 30 minutes. Add finely chopped basil 2-3 minutes before the sauce is done.",
+      "Preparation of the sauce — Bechamel. Pour the milk into a saucepan and heat without bringing to boil. The milk should be hot. Melt the butter in a stewpan, add the flour, and fry for 2-3 minutes. Then, gradually pour in the hot milk, stirring to avoid lumps. Continue to stir and simmer, add salt and nutmeg. Once the sauce has thickened, it’s ready. Remove it from heat.",
+      "Laying out the lasagna. Grate cheese. Preheat oven to 428°F (220°C). Oil a casserole dish and pour in some Bechamel sauce to cover the bottom, then pour in some Bolognese sauce following the same principle.",
+      "Lay out 4 lasagna noodles with overlapping joints. Pour the sauces on top, and sprinkle with cheese. Repeat this process until ingredients are gone.",
+      "Put the dish in the preheated oven for 20 minutes. Turn off the oven, cover the dish with foil and leave in a hot oven for another 10 minutes. Enjoy!"
+    ]
+  }
 ]
 
 // find recipes that user has ingredients for
@@ -252,8 +272,7 @@ function matchedIngredients(recipeIngredients, userIngredients) {
   var matchCount = 0;
   userIngredients.forEach(function(userIngredient) {
     recipeIngredients.forEach(function(recipeIngredient) {
-      // if (userIngredient === recipeIngredient) {
-        if (userIngredient.includes(recipeIngredient)) {
+      if (userIngredient === recipeIngredient) {
         matchCount++;
       }
     })
@@ -356,6 +375,8 @@ $(document).ready(function() {
       if (matchedRecipe)
       {
         var nameFormatted = formatForFrontend(recipe.name);
+        console.log(matchedRecipe)
+        console.log(matchedRecipes)
         var recipeID = recipe.name+'_id'+recipe.id;
         matchedRecipes.push(recipe)
         $("#matched-recipes").append(
